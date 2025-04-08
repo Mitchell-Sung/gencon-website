df778557890ae284c095dbce518fabdb93ee13c7
Mitchell-Sung <mitchell.sung@gmail.com>
2025-04-07 20:30:04 -0600

    feat: complete the Contact button to link to the contact page

--- src/containers/Header.tsx
+++ src/containers/Header.tsx
@@ -1,16 +1,19 @@
-import { Container, Section, Img, Content } from '../components/Tags';
-import logo_01 from '../assets/logo_01.png';
 import { NavButton } from '../components';
-import { navBtnData } from '../data';
-import st from '../styles/containers/Header.module.css';
-import useFirstPathSegment from '../hooks/useFirstPathSegment';
+import { Container, Section, Img, Content } from '../components/Tags';
 import { Button } from '../components/mui';
+import useFirstPathSegment from '../hooks/useFirstPathSegment';
+import { navBtnData } from '../data';
+import logo_01 from '../assets/logo_01.png';
+import st from '../styles/containers/Header.module.css';
+import useNavigateTo from '../hooks/useNavigateTo';
+import { ROUTE } from '../consts';
 
 function Header() {
     const pathname = useFirstPathSegment();
+    const navigateToUrl = useNavigateTo();
 
     return (
-        <Container className={st.container}>
+        <Container className={st.header}>
             <Img className={st.logo} src={logo_01} alt='Logo' />
             <Section className={st.section}>
                 <Content className={st.content}>
@@ -23,7 +26,11 @@ function Header() {
                         />
                     ))}
                 </Content>
-                <Button className={st.button} variant='contained'>
+                <Button
+                    className={st.button}
+                    variant='contained'
+                    onClick={() => navigateToUrl(ROUTE.CONTACT)}
+                >
                     Contact
                 </Button>
             </Section>
--- src/containers/Layout.tsx
+++ src/containers/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './';
 import { Container, Section } from '../components/Tags';
+import { Divider } from '../components/mui';
 
 function Layout() {
     return (
         <Container>
             <Header />
+            <Divider variant='middle' />
             <Section>
                 <Outlet />
             </Section>
